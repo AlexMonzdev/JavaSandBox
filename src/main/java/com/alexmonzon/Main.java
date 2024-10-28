59ff64661a4029e00e65ee9bc67f545792dec288
AlexMonzdev <ialexmonzon@gmail.com>
2024-10-28 10:05:21 +0100

    hola mundo java

--- src/main/java/com/alexmonzon/Main.java
+++ src/main/java/com/alexmonzon/Main.java
@@ -3,7 +3,7 @@ package com.alexmonzon;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello coders");
 
             }
 
