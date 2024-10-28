c15922b1e9cd6de21daeeacfbde81ea962fe7437
AlexMonzdev <ialexmonzon@gmail.com>
2024-10-28 11:18:29 +0100

    tipos primitivos

--- src/main/java/com/alexmonzon/Main.java
+++ src/main/java/com/alexmonzon/Main.java
@@ -3,8 +3,18 @@ package com.alexmonzon;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello coders");
+        boolean bool = true;
+        byte numbyte = 0;
+        int numint = 1;
+        short numshort = 2;
+        double numdouble = 2.5;
+        float numbfoat = 0.5f;
+        long numlong = 25L;
+        char numchar = 'A';
 
-            }
+        System.out.printf("boolean = " + bool + "\nbyte = " + numbyte + "\nint = " + numint + "\nshort = " + numshort + "\n"
+                + "double = " + numdouble + "\nlong = " + numlong+"\nFloat = "+numbfoat+"\nChart = "+ numchar);
+
+    }
 
 }
