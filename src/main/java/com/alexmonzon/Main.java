39c7ee413f46f34e82f259ca5ae936eab66abf06
AlexMonzdev <ialexmonzon@gmail.com>
2024-10-28 13:06:43 +0100

    arrays

--- src/main/java/com/alexmonzon/Main.java
+++ src/main/java/com/alexmonzon/Main.java
@@ -3,8 +3,8 @@ package com.alexmonzon;
 public class Main {
     public static void main(String[] args) {
 
-      E04_BucleWhile uno = new E04_BucleWhile();
-      uno.bucleWhile();
+      E05_Arrays uno = new E05_Arrays();
+      uno.acceso();
 
 
 
